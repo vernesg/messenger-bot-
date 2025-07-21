03f6a2fa34aad70016d2147a4f75a6630b1a78c8
vernesg <veaxdev36@gmail.com>
2025-07-22 00:09:05 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "help",
-  description: "Lists all commands",
+  description: "Show all commands",
   execute(api, event) {
-    const helpMsg = "🧠 Available commands:\n- hi\n- help\n- echo <message>";
-    api.sendMessage(helpMsg, event.threadID);
+    const helpText = "🧠 Commands:\n- hi\n- help\n- echo <text>";
+    api.sendMessage(helpText, event.threadID);
   }
 };
